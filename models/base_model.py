9fada617c8bbbe8f34b920e67a46a3e01cd14d8f
Romha Shwangzaw <romhas@gmail.com>
2022-11-21 22:11:47 +0300

    task 5 done

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -24,10 +25,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         ''' updates `updated_at` with the current time '''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         ''' returns a dictionary containing all keys/values
